47da50ea8922e07b0b4d59c9aee51df77cb7253a
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-21 13:13:05 +0530

    Implement socket for real time Auction details

--- Controllers/LiveAuctionController.js
+++ Controllers/LiveAuctionController.js
@@ -1,1 +1,18 @@
-const Auction = require('../Models/auctionModel')
+// const Auction = require('../Models/auctionModel')
+
+// const { io }= require('../index.js')
+
+// // const io = require('socket.io')(server, {
+// //     cors: {
+// //       origin: '*',
+// //     }
+// //   });
+
+//   io.on('connection', (socket) => {
+//     socket.on('detail_to_server', (message) => {
+//         console.log(message)
+//       io.emit('detail_to_all', message);
+//     });
+//     socket.on('disconnect', () => {
+//     });
+//   });
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Making connection to mongo
 const connectToMongo = require('./db');
+const http = require('http');
+
 
 //Calling function to check if the database is connected
 connectToMongo();
@@ -12,9 +14,59 @@ const port = 3003
 
 const app = express()
 
+let timerValue = 10000000000;
+let currentbid=0;
+let nextbid=0;
+
 //Using a express middleware to enable req.body
 app.use(express.json());
 app.use(cors())
+const socketIO = require('socket.io');
+
+timerInterval = setInterval(() => {
+    timerValue-=1;
+    // console.log(timerValue)
+  if (timerValue <= 0) {
+    clearInterval(timerInterval); 
+    console.log('Timer reached zero');
+  }
+}, 1000);
+
+
+const server = http.createServer(express());
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+
+
+io.on('connection', (socket) => {
+
+  socket.on("join", (token) => {
+    const details={timer:timerValue, bidamount:currentbid, nextbid:nextbid};
+    socket.emit('detail_to_all', details);
+  });
+
+  socket.on('detail_to_server', (message) => {
+    console.log(message);
+    timerValue=message.timer;
+    currentbid=message.bidamount
+    nextbid=message.nextbid
+    io.emit('detail_to_all', message);
+  });
+  
+  socket.on('disconnect', () => {
+    // Handle disconnect event if needed
+  });
+});
+
+// Rest of your backend code...
+
+server.listen(3000, () => {
+  console.log(`Server listening on http://localhost:3003`);
+});
 
 
 //Available routes whose api can be hit

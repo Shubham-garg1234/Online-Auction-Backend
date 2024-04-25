402b0395b7d928dcfe2e327d0e0b6dd032c28bab
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-25 17:23:39 +0530

    Resolved issue of deployment in google auth

--- Routes/googleRoutes.js
+++ Routes/googleRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const session = require("express-session")
-require("../MiddleWares/Passport")
+require("../Middlewares/Passport")
 const googleController = require('../Controllers/googleController')
 
 

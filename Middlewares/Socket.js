96c67abc1d9670b2384a45801e6e03d68df6ea09
myusername <127469454+Shubham-garg1234@users.noreply.github.com>
2024-04-27 23:52:46 +0530

    debugging timer

--- Middlewares/Socket.js
+++ Middlewares/Socket.js
@@ -150,7 +150,7 @@ let currentbid=0;
 let nextbid=0;
 
 timerInterval = setInterval(() => {
-  //console.log(timerValue);
+  console.log(timerValue);
   timerValue-=1;
   if(timerValue!=0 && timerValue%10000==0){
     async function start2(){
